cdd1bf4449762181c492fff96ad503a14b4d391d
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-01-13 14:51:41 +0700

    update form

--- forms/contact.js
+++ forms/contact.js
@@ -1,9 +1,8 @@
 document.getElementById("contact-form").addEventListener("submit", function (e) {
         e.preventDefault(); // Prevent the default form submission
-        document.getElementById("message").textContent = "Submitting..";
-        document.getElementById("message").style.display = "block";
-        document.getElementById("submit-button").disabled = true;
-
+        document.querySelector('.loading').classList.add('d-block');
+        document.querySelector('.error-message').classList.remove('d-block');
+        document.querySelector('.sent-message').classList.remove('d-block');
         // Collect the form data
         var formData = new FormData(this);
         var keyValuePairs = [];
@@ -30,29 +29,40 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
             if (response) {
               return response; // Assuming your script returns JSON response
             } else {
+              displayError(document, "Failed to submit the form");
               throw new Error("Failed to submit the form.");
             }
           })
           .then(function (data) {
+            document.querySelector('.loading').classList.remove('d-block');
+            document.querySelector('.sent-message').classList.add('d-block');
+            document.getElementById("contact-form").reset(); 
             // Display a success message
-            document.getElementById("message").textContent =
-              "Data submitted successfully!";
-            document.getElementById("message").style.display = "block";
-            document.getElementById("message").style.backgroundColor = "green";
-            document.getElementById("message").style.color = "beige";
-            document.getElementById("submit-button").disabled = false;
-            document.getElementById("contact-form").reset();
+            // document.getElementById("message").textContent =
+            //   "Data submitted successfully!";
+            // document.getElementById("message").style.display = "block";
+            // document.getElementById("message").style.backgroundColor = "green";
+            // document.getElementById("message").style.color = "beige";
+            // document.getElementById("submit-button").disabled = false;
+            // document.getElementById("contact-form").reset();
 
-            setTimeout(function () {
-              document.getElementById("message").textContent = "";
-              document.getElementById("message").style.display = "none";
-            }, 2600);
+            // setTimeout(function () {
+            //   document.getElementById("message").textContent = "";
+            //   document.getElementById("message").style.display = "none";
+            // }, 2600);
           })
           .catch(function (error) {
             // Handle errors, you can display an error message here
-            console.error(error);
-            document.getElementById("message").textContent =
-              "An error occurred while submitting the form.";
-            document.getElementById("message").style.display = "block";
+            // console.error(error);
+            displayError(document, error);
+            // document.getElementById("message").textContent =
+            //   "An error occurred while submitting the form.";
+            // document.getElementById("message").style.display = "block";
           });
+
+          function displayError(thisForm, error) {
+            thisForm.querySelector('.loading').classList.remove('d-block');
+            thisForm.querySelector('.error-message').innerHTML = error;
+            thisForm.querySelector('.error-message').classList.add('d-block');
+          }
       });
